5bf04f229b22dcba29a4a0dcfa1e60a431d7f553
Hisaalu <hisaaalujuniornelson@gmail.com>
2026-03-29 14:59:29 +0300

    email configs

--- config/config.php
+++ config/config.php
@@ -8,6 +8,15 @@ define('DB_NAME', getenv('DB_NAME') ?: 'ROGELEDB');
 define('DB_USER', getenv('DB_USER') ?: '2VcYykLWVZacLnw.root');
 define('DB_PASS', getenv('DB_PASS') ?: '');
 
+// Mail Configuration - Using environment variables from Render
+define('MAIL_HOST', getenv('MAIL_HOST') ?: 'mail.privateemail.com');
+define('MAIL_PORT', getenv('MAIL_PORT') ?: 587);
+define('MAIL_USERNAME', getenv('MAIL_USERNAME') ?: 'info@raysofgrace.ac.ug');
+define('MAIL_PASSWORD', getenv('MAIL_PASSWORD') ?: '');
+define('MAIL_ENCRYPTION', getenv('MAIL_ENCRYPTION') ?: 'tls');
+define('MAIL_FROM_ADDRESS', getenv('MAIL_FROM_ADDRESS') ?: 'info@raysofgrace.ac.ug');
+define('MAIL_FROM_NAME', getenv('MAIL_FROM_NAME') ?: 'ROGELE');
+
 // Application Configuration - Using environment variables
 define('BASE_URL', getenv('APP_URL') ?: 'https://rogele.onrender.com');
 define('SITE_NAME', getenv('APP_NAME') ?: 'ROGELE');
