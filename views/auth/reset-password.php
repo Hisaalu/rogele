c322ca8be530069f8f28f1d13afcef68112862c3
Hisaalu <hisaaalujuniornelson@gmail.com>
2026-05-31 14:17:52 +0300

    changed favicon from png to jpg

--- views/auth/reset-password.php
+++ views/auth/reset-password.php
@@ -15,7 +15,7 @@ $token = $token ?? '';
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     
     <!-- Favicon -->
-    <link rel="icon" type="image/png" href="<?php echo BASE_URL; ?>/public/images/logo.png">
+    <link rel="icon" type="image/png" href="<?php echo BASE_URL; ?>/public/images/logo.jpg">
     
     <style>
         * {
