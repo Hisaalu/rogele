59049e7caae55d80ffc31b1e4f52551fee7fd62d
Hisaalu <hisaaalujuniornelson@gmail.com>
2026-07-08 10:49:45 +0300

    fix: database connection failure on render envronment

--- config/config.php
+++ config/config.php
@@ -33,7 +33,7 @@ define('DB_HOST', getenv('DB_HOST') ?: '127.0.0.1');
 define('DB_PORT', getenv('DB_PORT') ?: '4000');
 define('DB_NAME', getenv('DB_NAME') ?: '');
 define('DB_USER', getenv('DB_USER') ?: '');
-define('DB_PASS', getenv('DB_PASS') !== false ? getenv('DB_PASS') : ''); 
+define('DB_PASS', getenv('DB_PASS') ?: '');
 
 $appUrl = getenv('APP_URL') ?: (getenv('RENDER') ? 'https://rogele.raysofgrace.ac.ug' : 'http://localhost/rogele-prod');
 define('BASE_URL', rtrim($appUrl, '/'));
