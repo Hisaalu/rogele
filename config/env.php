76f95be353894353a03a6faa312b1ce092b3d34d
Hisaalu <hisaaalujuniornelson@gmail.com>
2026-07-27 17:58:25 +0300

    modified app url

--- config/env.php
+++ config/env.php
@@ -3,7 +3,7 @@
 
 define('APP_NAME', getenv('APP_NAME') ?: 'ROGELE');
 define('APP_ENV', getenv('APP_ENV') ?: 'production');
-define('APP_URL', getenv('APP_URL') ?: 'http://rogele.raysofgrace.ac.ug');
+define('APP_URL', getenv('APP_URL') ?: 'https://rogele-payments.onrender.com/');
 
 define('MAIL_HOST', getenv('MAIL_HOST') ?: 'mail.privateemail.com');
 define('MAIL_PORT', getenv('MAIL_PORT') ?: 587);
